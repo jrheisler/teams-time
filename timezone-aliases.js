bd1045ac62efae5b426e1eb1304e43321ae149ac
Jeff Heisler <jrheisler@yahoo.com>
2025-09-27 15:35:28 -0700

    Add Alexandria timezone alias

--- timezone-aliases.js
+++ timezone-aliases.js
@@ -1,4 +1,6 @@
 const timezoneAliases = {
+  'Alexandria Egypt': 'Africa/Cairo',
+  Alexandria: 'Africa/Cairo',
   Belgium: 'Europe/Brussels',
   Bishkek: 'Asia/Bishkek',
   Uzbekistan: 'Asia/Tashkent'
